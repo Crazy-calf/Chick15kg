d96fdea7c13bd68c64d745db30797e2b526215ce
Crazy-calf <1061667048@qq.com>
2019-06-14 17:20:08 +0800

    添加项目

---
+++ Tools/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public static class Tools
+    {
+        public static T[,] ConvertToArray<T>(this T[][] data)
+        {
+            T[,] res = new T[data.Length, data[0].Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++) res[i, j] = data[i][j];
+            }
+            return res;
+        }
+    }
+}
